70401ffbe7aaa59ad917cd6cb384c63aab231c98
YerinJun <erin2jj@gmail.com>
2021-07-18 20:56:49 +0900

    7/18

---
+++ app/src/main/java/review/App3.java
@@ -0,0 +1,13 @@
+package review;
+
+public class App3 {
+  public static void main(String[] args) {
+    System.out.printf("[%s]\n", "작업");
+    System.out.printf("%s: %s\n", "프로젝트", "미니 프로젝트 관리 시스템 개발");
+    System.out.printf("%s: %d\n", "번호", 1);
+    System.out.printf("%s: %s\n", "내용", "요구사항 수집");
+    System.out.printf("%s: %s\n", "완료일", "2020-01-20");
+    System.out.printf("%s: %s\n", "상태", "진행중");
+    System.out.printf("%s: %s\n", "담당자", "홍길동");
+  }
+}
