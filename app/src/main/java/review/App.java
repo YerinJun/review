edd84966f9deba55f6c09737380480653f9d8d87
YerinJun <erin2jj@gmail.com>
2021-07-24 17:51:55 +0900

    -02-c-1~8

--- app/src/main/java/review/App.java
+++ app/src/main/java/review/App.java
@@ -7,21 +7,21 @@ public class App {
   public static void main(String[] args) {
     System.out.println("[회원]");
 
-    final int MAX_LENGTH = 100;
-
-    int[] no = new int[MAX_LENGTH];
-    String[] name = new String[MAX_LENGTH];
-    String[] email = new String[MAX_LENGTH];
-    String[] pw = new String[MAX_LENGTH];
-    String[] photo = new String[MAX_LENGTH];
-    String[] tel = new String[MAX_LENGTH];
-    Date[] registeredDate = new Date[MAX_LENGTH];
-
     Scanner keyboard = new Scanner(System.in);
 
-    int size = 0; 
+    final int MAX_VALUE = 100;
 
-    for (int i = 0; i < MAX_LENGTH; i++) {
+    int[] no = new int[MAX_VALUE];
+    String[] name = new String[MAX_VALUE];
+    String[] email = new String[MAX_VALUE];
+    String[] password = new String[MAX_VALUE];
+    String[] photo = new String[MAX_VALUE];
+    String[] tel = new String[MAX_VALUE];
+    Date[] registeredDate = new Date[MAX_VALUE];
+
+    int size = 0;
+
+    for (int i = 0; i < MAX_VALUE; i++) {
       System.out.print("번호? ");
       no[i] = Integer.parseInt(keyboard.nextLine());
       System.out.print("이름? ");
@@ -29,27 +29,26 @@ public class App {
       System.out.print("이메일? ");
       email[i] = keyboard.nextLine();
       System.out.print("암호? ");
-      pw[i] = keyboard.nextLine();
+      password[i] = keyboard.nextLine();
       System.out.print("사진? ");
       photo[i] = keyboard.nextLine();
       System.out.print("전화? ");
       tel[i] = keyboard.nextLine();
       registeredDate[i] = new Date();
-      size = size + 1;
-      System.out.println();
+      size += 1;
 
-      System.out.println("계속 입력하시겠습니까? (y/N)");
+      System.out.print("계속 입력하시겠습니까?(y/N)");
       String input = keyboard.nextLine();
-      if (input.equalsIgnoreCase("n") || input.equals("")) {
+      if (input.equalsIgnoreCase("n") || input.length() == 0) {
         break;
       }
     }
     keyboard.close();
 
-    System.out.println("--------------------------------------------");
-
-    for (int i = 0; i < size; i++) {
-      System.out.printf("%s, %s, %s, %s, %s\n", no[i], name[i], email[i], tel[i], registeredDate[i]);
+    System.out.println("----------------------------------------------");
+    for(int i = 0; i < size; i++) {
+      System.out.printf("%d, %s, %s, %s, %tY-%5$tm-%5$td\n",
+          no[i], name[i], email[i], tel[i], registeredDate[i]);
     }
   }
 }
