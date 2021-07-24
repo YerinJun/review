3aed0f9592ac78a717d4f2a061ec08692720dff3
YerinJun <erin2jj@gmail.com>
2021-07-24 17:20:48 +0900

    02-c-condition size

--- app/src/main/java/review/App.java
+++ app/src/main/java/review/App.java
@@ -7,7 +7,7 @@ public class App {
   public static void main(String[] args) {
     System.out.println("[회원]");
 
-    final int MAX_LENGTH = 5;
+    final int MAX_LENGTH = 100;
 
     int[] no = new int[MAX_LENGTH];
     String[] name = new String[MAX_LENGTH];
@@ -19,8 +19,9 @@ public class App {
 
     Scanner keyboard = new Scanner(System.in);
 
+    int size = 0; 
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < MAX_LENGTH; i++) {
       System.out.print("번호? ");
       no[i] = Integer.parseInt(keyboard.nextLine());
       System.out.print("이름? ");
@@ -34,13 +35,20 @@ public class App {
       System.out.print("전화? ");
       tel[i] = keyboard.nextLine();
       registeredDate[i] = new Date();
+      size = size + 1;
       System.out.println();
+
+      System.out.println("계속 입력하시겠습니까? (y/N)");
+      String input = keyboard.nextLine();
+      if (input.equalsIgnoreCase("n") || input.equals("")) {
+        break;
+      }
     }
     keyboard.close();
 
     System.out.println("--------------------------------------------");
 
-    for (int i = 0;i < 5; i++) {
+    for (int i = 0; i < size; i++) {
       System.out.printf("%s, %s, %s, %s, %s\n", no[i], name[i], email[i], tel[i], registeredDate[i]);
     }
   }
