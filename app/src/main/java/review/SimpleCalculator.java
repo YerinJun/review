dd02048eada0d2ed439d9635d008d533dd69fb9f
YerinJun <erin2jj@gmail.com>
2021-07-25 14:53:00 +0900

    enhanced SimpleCarculator

--- app/src/main/java/review/SimpleCalculator.java
+++ app/src/main/java/review/SimpleCalculator.java
@@ -25,6 +25,7 @@ public class SimpleCalculator {
       result = v1 / v2;
     } else {
       System.out.println("Please, choose one of +, -, *, / operators.");
+      return;
     }
 
     System.out.printf("%d %s %d = %d", v1, operator, v2, result);
