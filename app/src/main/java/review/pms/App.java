e094a055e3f42b7897e6a251f94324f85213732b
YerinJun <erin2jj@gmail.com>
2021-08-03 20:40:34 +0900

    06-a CRUD

--- app/src/main/java/review/pms/App.java
+++ app/src/main/java/review/pms/App.java
@@ -26,11 +26,29 @@ public class App {
       } else if (input.equals("/member/list")) {
         memberHandler.list();
 
+      } else if (input.equals("/member/detail")) {
+        memberHandler.detail();
+
+      } else if (input.equals("/member/update")) {
+        memberHandler.update();
+
+      } else if (input.equals("/member/delete")) {
+        memberHandler.delete();
+
       } else if (input.equals("/project/add")) {
         projectHandler.add(memberHandler);
 
       } else if (input.equals("/project/list")) {
         projectHandler.list();
+
+      } else if (input.equals("/project/detail")) {
+        projectHandler.detail();
+
+      } else if (input.equals("/project/update")) {
+        projectHandler.update(memberHandler);
+
+      } else if (input.equals("/project/delete")) {
+        projectHandler.delete();
 
       } else if (input.equals("/task/add")) {
         taskHandler.add(memberHandler);
@@ -38,11 +56,29 @@ public class App {
       } else if (input.equals("/task/list")) {
         taskHandler.list();
 
+      } else if (input.equals("/task/detail")) {
+        taskHandler.detail();
+
+      } else if (input.equals("/task/update")) {
+        taskHandler.update(memberHandler);
+
+      } else if (input.equals("/task/delete")) {
+        taskHandler.delete();
+
       } else if (input.equals("/board/add")) {
         boardHandler.add();
 
       } else if (input.equals("/board/list")) {
         boardHandler.list();
+
+      } else if (input.equals("/board/detail")) {
+        boardHandler.detail();
+
+      } else if (input.equals("/board/update")) {
+        boardHandler.update();
+
+      } else if (input.equals("/board/delete")) {
+        boardHandler.delete();
 
       } else {
         System.out.println("유효하지 않은 명령입니다.");
